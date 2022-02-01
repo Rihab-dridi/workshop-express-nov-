8c04928041c0bd967adaf75c2c6d5d33ef0a2a2c
RIHAB <rihab.dridi.igs@gmail.com>
2022-02-01 11:51:20 +0100

    express Router

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
-//----*create a server 
+//----*create a server
 //import express
-const express=require('express')
-const { sendFile } = require('express/lib/response')
-//create an instance for express 
-const app=express()
+const express = require("express");
+const { sendFile } = require("express/lib/response");
+//create an instance for express
+const app = express();
 //create a port
-const port=5000
+const port = 5000;
 
 //----* middleware
- //type1: third party middleware:
- const cors=require('cors')
- const multer=require('multer')
+//type1: third party middleware:
+const cors = require("cors");
+const multer = require("multer");
 
+//type2: built-in middelware
+app.use(express.static("public"));
+app.use(express.json())
+app.use("/api/users", require("./routes/users"));
+app.use("/api/books", require("./routes/books"));
 
-  //type2: built-in middelware 
-  app.use(express.static('public'))
+//type3: custum middleware
 
-
-  //type3: custum middleware
-
-  const middleExp=(req, res, next)=>{
-console.log(' this is from the middleware ')
-next()
-  }
-  app.use(middleExp)
-//----*route 
+const middleExp = (req, res, next) => {
+  console.log(" this is from the middleware ");
+  next();
+};
+app.use(middleExp);
+//----*route
 // app.get('/', (req,res)=>{
 // res.send("hello from express")
 // })
 
-app.get('/', (req,res)=>{
-res.sendFile(__dirname+'/public/home.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/home.html");
+});
 
-app.use(middleExp)
-app.get('/about',middleExp,  (req,res)=>{
-res.sendFile(__dirname+'/public/about.html')
-})
+app.use(middleExp);
+app.get("/about", middleExp, (req, res) => {
+  res.sendFile(__dirname + "/public/about.html");
+});
 
-
-
-
-//listen to the port 
-app.listen(port,(err)=>{
-    err? console.log(err):
-console.log(`the server is up and running on${port} `)
-} )
+//listen to the port
+app.listen(port, (err) => {
+  err
+    ? console.log(err)
+    : console.log(`the server is up and running on${port} `);
+});
---
+++ routes/books.js
@@ -0,0 +1,10 @@
+const express=require('express')
+//import Router middleware
+const router=express.Router()
+
+
+
+
+
+//export router middleware 
+module.exports=router
